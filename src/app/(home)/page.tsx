4038b3b14509bb46baaadfb2fd23fb3cedf24fe2
Arash <arash382.jb@gmail.com>
2023-08-07 20:58:54 +0400

    improve homepage component structure

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HeaderSpace from "~/app/_components/HeaderSpace";
+import Hero from "./_components/Hero";
+
+const Page = () => {
+  return (
+    <div className="flex flex-col">
+      <HeaderSpace />
+
+      <Hero />
+    </div>
+  );
+};
+
+export default Page;
