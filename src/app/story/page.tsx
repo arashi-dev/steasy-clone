235184030419de441d1bc9d53b6ab95d650ad8a3
Arash <arash382.jb@gmail.com>
2023-08-10 00:18:40 +0400

    add founders quote to story page

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -16,6 +16,18 @@ const Page: React.FC = () => {
       <Container>
         <Image src={FoundersImage} alt="founders" className="rounded-lg" />
       </Container>
+
+      <Container className="py-20 lg:py-40">
+        <p className="mx-auto w-full text-center font-gothic text-2xl/tight font-bold md:w-10/12 md:text-4xl/tight lg:w-8/12 lg:text-[2.5rem]/tight">
+          “Wir sind überzeugt, dass die wichtigste Mahlzeit am Tag ein warmer
+          Lunch ist, denn er gibt Energie für Körper und Geist. Es wird Zeit,
+          ihm mehr gerecht zu werden.”
+        </p>
+
+        <p className="mt-7 text-center font-gothic text-lg text-ginger-500 md:text-xl lg:mt-12 lg:text-2xl">
+          Johannes, Claudio und Reto
+        </p>
+      </Container>
     </div>
   );
 };
