b240b6d554e773e59c19d3902b97ef0e5488b3e7
Arash <arash382.jb@gmail.com>
2023-08-10 00:07:03 +0400

    add founders image to story page

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Hero from "./_components/Hero";
 import Founders from "./_components/Founders";
+import Container from "../_components/Container";
+import Image from "next/image";
+
+import FoundersImage from "./_components/founders.jpg";
 
 const Page: React.FC = () => {
   return (
@@ -8,6 +12,10 @@ const Page: React.FC = () => {
       <Hero />
 
       <Founders />
+
+      <Container>
+        <Image src={FoundersImage} alt="founders" className="rounded-lg" />
+      </Container>
     </div>
   );
 };
